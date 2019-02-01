7700875954fbe979eeeabc3097dd8af50b3141c2
Gaorieh Isaac <gisaac@olbico.nl>
2019-02-01 16:36:19 +0100

    gg

---
+++ test3/ClientApp/boot.browser.js
@@ -0,0 +1,31 @@
+import '@angular/common/locales/en';
+import '@angular/common/locales/fr';
+import '@angular/common/locales/nl';
+import "jquery/dist/jquery";
+import "signalr";
+////////////////////////////////////////////////
+import "datatables.net";
+import "datatables.net-plugins/pagination/input.js";
+import "datatables.net-select-dt";
+////////////////////////////////////////////
+import { enableProdMode } from '@angular/core';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { AppModule } from './app/app.module';
+//declare var module: any;
+if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(function () {
+        // Before restarting the app, we create a new root element and dispose the old one
+        var oldRootElem = document.querySelector('app');
+        var newRootElem = document.createElement('app');
+        oldRootElem.parentNode.insertBefore(newRootElem, oldRootElem);
+        modulePromise.then(function (appModule) { return appModule.destroy(); });
+    });
+}
+else {
+    enableProdMode();
+}
+// Note: @ng-tools/webpack looks for the following expression when performing production
+// builds. Don't change how this line looks, otherwise you may break tree-shaking.
+var modulePromise = platformBrowserDynamic().bootstrapModule(AppModule);
+//# sourceMappingURL=boot.browser.js.map
